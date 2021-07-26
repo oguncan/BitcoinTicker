fb243aa7846043269edb53c709b01551c83fd426
Ogün Can KAYA <oguncankaya@bgnmobi.com>
2021-07-26 22:15:45 +0300

    -Updating the default theme -Creating the Login Activity and updating the package structure

--- app/src/main/java/mobi/okmobile/bitcointicker/ui/auth/LoginActivity.kt
+++ app/src/main/java/mobi/okmobile/bitcointicker/ui/auth/LoginActivity.kt
@@ -1,11 +1,12 @@
-package mobi.okmobile.bitcointicker
+package mobi.okmobile.bitcointicker.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mobi.okmobile.bitcointicker.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/mobi/okmobile/bitcointicker/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package mobi.okmobile.bitcointicker.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mobi.okmobile.bitcointicker.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
