b01beec97265a5c7aa6befb4c747e870a8dd9cd3
Ogün Can KAYA <oguncankaya@bgnmobi.com>
2021-07-27 21:26:00 +0300

    -Creating the Constant object class -Creating the required data model class for the data we will receive from the API -Writing the interface class for the values to be retrieved from the API -Writing the necessary sealed class to control the [success, error, loading] status of the data coming from the API

---
+++ app/src/main/java/mobi/okmobile/bitcointicker/api/models/HistoricalPriceResponse.kt
@@ -0,0 +1,5 @@
+package mobi.okmobile.bitcointicker.api.models
+
+data class HistoricalPriceResponse(
+    val prices: List<DoubleArray>
+)
---
+++ app/src/main/java/mobi/okmobile/bitcointicker/util/Constants.kt
@@ -0,0 +1,5 @@
+package mobi.okmobile.bitcointicker.util
+
+object Constants {
+    const val GENERIC_ERROR = "Something went wrong, try again!"
+}
